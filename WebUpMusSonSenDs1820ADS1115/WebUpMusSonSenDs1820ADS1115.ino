86257f9b2710ddba944581bd74665058f65e7dc0
zxsony <zxsony@list.ru>
2023-06-11 01:08:26 +0300

    Версия

--- WebUpMusSonSenDs1820ADS1115/WebUpMusSonSenDs1820ADS1115.ino
+++ WebUpMusSonSenDs1820ADS1115/WebUpMusSonSenDs1820ADS1115.ino
@@ -24,7 +24,7 @@ const char* host = "esp8266-webupdate";
 //#define debug2320
 //String deviceId = "1";  //1=loc, sound; 2=loc, temp
                         //3=pn, temp; 4=td, temp
-String ver = "v2.5.29";
+String ver = "v2.6.0";
 //////////////////////
 
 
